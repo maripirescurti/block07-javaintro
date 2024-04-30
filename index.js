2b09a325fe2c70af3fd7d7d62cd95825bff56ca3
Mariana Pires Curti <marianapirescurti@MacBook-Pro.lan>
2024-04-29 23:39:32 -0400

    added java code

---
+++ index.js
@@ -0,0 +1,21 @@
+// created a message
+console.log('You have received this message becasue you have been chosen to open an important vault Here is the secret combination:')
+
+// created a variable with a division opearation 10
+let firstNum = 200 / 20
+
+// created a variable with an addition 40
+let secondNum = 35 + 5
+
+// created a variable with a multiplication 39
+let thirdNum = 7 * 7
+
+// created console logs to check the code numbers
+console.log(firstNum)
+console.log(secondNum)
+console.log(thirdNum)
+
+// created an alert with the final vault code
+const combinedNums = `${firstNum} ${secondNum} ${thirdNum}`
+
+alert(combinedNums)
