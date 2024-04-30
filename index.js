ae44c91bd50cc76ca30b5c6ca05bd5c9d78c9545
Mariana Pires Curti <marianapirescurti@MacBook-Pro.lan>
2024-04-29 23:45:27 -0400

    corrected numbers and spellcheck

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 // created a message
-console.log('You have received this message becasue you have been chosen to open an important vault Here is the secret combination:')
+console.log('You have received this message because you have been chosen to open an important vault. Here is the secret combination:')
 
-// created a variable with a division opearation 10
+// created a variable with a division opearation that equals 10
 let firstNum = 200 / 20
 
-// created a variable with an addition 40
-let secondNum = 35 + 5
+// created a variable with a multiplication operation that equals 40
+let secondNum = 5 * 8
 
-// created a variable with a multiplication 39
-let thirdNum = 7 * 7
+// created a variable with a addition operation that equals 39
+let thirdNum = 28 + 11
 
 // created console logs to check the code numbers
 console.log(firstNum)
 console.log(secondNum)
 console.log(thirdNum)
 
-// created an alert with the final vault code
+//concatenated the three numerical operation variables in a string 
 const combinedNums = `${firstNum} ${secondNum} ${thirdNum}`
 
+// created an alert with the final vault code from the concatenation displaying as a pop-up
 alert(combinedNums)
